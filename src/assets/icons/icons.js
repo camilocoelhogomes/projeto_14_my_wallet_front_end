4be4349a0ac0602c30d724b02c57efd3edb179c1
Nivea <nivea@respondeai.com.br>
2021-10-29 12:06:01 -0300

    preparando para o review

--- src/assets/icons/icons.js
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as HeaderIcon } from './Vector.svg';
-import { ReactComponent as MinusIcon } from './ant-design_minus-circle-outlined.svg';
-import { ReactComponent as PlusIcon } from './ant-design_plus-circle-outlined.svg';
-
-export {
-    HeaderIcon,
-    MinusIcon,
-    PlusIcon
-}
